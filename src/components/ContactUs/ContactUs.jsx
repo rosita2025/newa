64358828a875b9fc7e9ba1168dd2f80c443639d1
rosita2025 <196705675+rosita2025@users.noreply.github.com>
2025-04-14 21:03:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+    return (
+        <div className="contact-us-container">
+            <h2>Contact Us</h2>
+            <p>Email: <a href="mailto:contact@kakashow.com">contact@kakashow.com</a></p>
+            <p>Phone: <a href="tel:+15169298899">+15169298899</a></p>
+        </div>
+    );
+};
+
+export default ContactUs;
